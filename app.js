9789573696a5cd06d98f29d5da567386199d6845
Varun Kumar <varunksidhwani@gmail.com>
2021-09-17 16:05:33 +0530

    FirstCommit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app  =  express()
+// console.dir(app)
+
+app.use((req, res) => {
+    console.log("Hello this is Varun Sidhwani")
+    // console.dir(req)
+    res.send("Hello Varun")
+})
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000")
+})
