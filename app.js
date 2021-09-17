103d03fb716d32aae652bb80a64e7c9bf82376fb
Varun Kumar <varunksidhwani@gmail.com>
2021-09-17 20:18:06 +0530

    addedEJSTemplating

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
-const app  =  express()
+const app  =  express();
+const path = require('path');
 // console.dir(app)
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'))
 
 // app.use((req, res) => {
 //     console.log("Hello this is Varun Sidhwani")
@@ -11,7 +15,8 @@ const app  =  express()
 app.get('/r/:subreddit', (req,res) => {
     const { subreddit } = req.params
     const {name, age} = req.query
-    res.send(`<h1>This is a ${subreddit} subreddit </h1> and username is ${name} and age is ${age}`)
+    // res.send(`<h1>This is a ${subreddit} subreddit </h1> and username is ${name} and age is ${age}`)
+    res.render('home.ejs')
 })
 
 app.listen(3000, () => {
