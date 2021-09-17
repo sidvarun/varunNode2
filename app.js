275323be4d6640496388cc8f5e708144251549f7
Varun Kumar <varunksidhwani@gmail.com>
2021-09-17 18:49:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const express = require("express");
 const app  =  express()
 // console.dir(app)
 
-app.use((req, res) => {
-    console.log("Hello this is Varun Sidhwani")
-    // console.dir(req)
-    res.send({name : 'Varun'})
+// app.use((req, res) => {
+//     console.log("Hello this is Varun Sidhwani")
+//     // console.dir(req)
+//     res.send({name : 'Varun'})
+// })
+
+app.get('/r/:subreddit', (req,res) => {
+    const { subreddit } = req.params
+    const {name, age} = req.query
+    res.send(`<h1>This is a ${subreddit} subreddit </h1> and username is ${name} and age is ${age}`)
 })
 
 app.listen(3000, () => {
