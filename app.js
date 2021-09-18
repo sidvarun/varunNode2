e371ecb4a4c9782a0f90b5c817eff81a86b12d25
sidvarun <varunksidhwani@gmail.com>
2021-09-18 19:58:22 +0530

    uuid Package

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app  =  express();
 const path = require('path');
+const {v4 : uuid} = require('uuid');
 // console.dir(app)
 app.use(express.urlencoded({extended : true}))
+app.use(express.json({extended : true}))
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
 app.use(express.static(path.join(__dirname, 'public')))
